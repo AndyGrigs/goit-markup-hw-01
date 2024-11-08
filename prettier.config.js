9b94342335e41589a043a7462e5f58b410573b34
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2024-11-08 15:44:07 +0100

    inital commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+    bracketSpacing: true,
+    arrowParens: "always",
+  };
+  
